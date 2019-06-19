c0081e2d6618a6482458d5e9f9f5037904b21b69
Ivan Valadares <ivan.valadares@kubra.com>
2019-06-18 21:58:59 -0400

    Fixing port env

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var nodeSuggestiveSearch = require("node-suggestive-search").init();
             
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
 
 app.get('/q/', (req, res) =>{
 
@@ -27,7 +27,7 @@ nodeSuggestiveSearch.on("initialized", () => {
 
     nodeSuggestiveSearch.loadJson("./titles.json", "utf8", "id", "title").then(() => {
 
-        app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+        app.listen(port, () => console.log(`Server listening on port ${port}!`))
 
     });
 
