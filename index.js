53adfbd7847dc4934653121c573ad92a8485b447
Ivan Valadares <ivan.valadares@kubra.com>
2019-06-20 18:38:11 -0400

    testing

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const nodeSuggestiveSearch = require("node-suggestive-search").init();
-            
+
 const path = require("path");
 const express = require('express');
 const app = express();
@@ -7,31 +7,32 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/query/', (req, res) =>{
+app.get('/query/', (req, res) => {
 
     nodeSuggestiveSearch.query(req.query.q, true).then(data => {
-            res.json(data);
-        }
-    );
+        res.json(data);
+    });
 
 });
 
-app.get('/suggestWords/', (req, res) =>{
+app.get('/suggestWords/', (req, res) => {
 
     nodeSuggestiveSearch.getSuggestedWords(req.query.q).then(data => {
-            res.json(data);
-        }
-    );
+        res.json(data);
+    }).catch(err => {
+        console.log(err);
+    });
 
 });
 
 
-app.get('/suggestItems/', (req, res) =>{
+app.get('/suggestItems/', (req, res) => {
 
     nodeSuggestiveSearch.getSuggestedItems(req.query.q).then(data => {
-            res.json(data);
-        }
-    );
+        res.json(data);
+    }).catch(err => {
+        console.log(err);
+    });
 
 });
 
